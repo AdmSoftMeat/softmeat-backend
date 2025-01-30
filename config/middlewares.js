15c5231579d39b73f4dde2e19535b83d013444c5
ApoRam <apoena.ramos@gmail.com>
2025-01-30 19:19:53 -0300

    hospedagem

--- config/middlewares.js
+++ config/middlewares.js
@@ -22,11 +22,20 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['*'],
+      origin: [
+        'http://localhost:1337',
+        'http://localhost:3000',
+        'http://localhost:4321',
+        'https://softmeat.com.br',
+        'https://www.softmeat.com.br',
+        'https://softmeat-backend-production.up.railway.app',
+        'https://softmeat.pages.dev' // Domínio do Cloudflare Pages
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       keepHeaderOnError: true,
       credentials: true,
       maxAge: 31536000,
+      exposedHeaders: ['Content-Range', 'X-Content-Range']
     },
   },
   'strapi::poweredBy',
