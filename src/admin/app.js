88880802c7f7a03bbdb02950b2a074d5e3e7bddb
ApoRam <apoena.ramos@gmail.com>
2025-02-03 01:40:15 -0300

    strapiPT

--- src/admin/app.js
+++ src/admin/app.js
@@ -3,15 +3,16 @@ const config = {
   translations: {
     'pt-BR': {
       'app.components.LeftMenu.navbrand.title': 'SoftMeat Dashboard',
+      'Content Manager': 'Gerenciador de Conteúdo',
+      'app.components.LeftMenu.collection.title': 'Tipos de Coleção',
+      'Settings.permissions': 'Permissões',
+      'Settings.roles': 'Funções',
+      'Settings.users': 'Usuários',
+      'notification.success.saved': 'Salvo com sucesso',
+      'global.save': 'Salvar',
+      'global.delete': 'Deletar',
+      'global.back': 'Voltar',
+      'global.cancel': 'Cancelar',
     },
   },
-};
-
-const bootstrap = (app) => {
-  console.log(app);
-};
-
-export default {
-  config,
-  bootstrap,
 };
