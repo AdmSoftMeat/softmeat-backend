8103e09c67700de4e0f55d9b8993080ba28c3fac
ApoRam <apoena.ramos@gmail.com>
2025-04-18 19:08:28 -0300

    deploy log erros

--- config/plugins.js
+++ config/plugins.js
@@ -4,11 +4,12 @@ const path = require('path');
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-aws-s3',
       providerOptions: {
         s3Options: {
-          accessKeyId: env('R2_ACCESS_KEY'),
-          secretAccessKey: env('R2_SECRET_KEY'),
+          credentials: {
+            accessKeyId: env('R2_ACCESS_KEY'),
+            secretAccessKey: env('R2_SECRET_KEY')
+          },
           endpoint: env('R2_ENDPOINT'),
           region: env('R2_REGION', 'auto'),
           params: {
