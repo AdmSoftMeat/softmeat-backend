1349597e63aab0458e934e4f12c8dfab610fe613
AdmSoftMeat <smcloudfare@gmail.com>
2025-05-22 04:46:37 -0300

    migra 16

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,19 @@
+// config/plugins.js
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-aws-s3',
+      provider: "aws-s3",
       providerOptions: {
-        accessKeyId: env('R2_ACCESS_KEY'),
-        secretAccessKey: env('R2_SECRET_KEY'),
-        endpoint: env('R2_ENDPOINT'),
-        region: 'auto',
+        accessKeyId: env("R2_ACCESS_KEY"),
+        secretAccessKey: env("R2_SECRET_KEY"),
+        endpoint: env("R2_ENDPOINT"),
+        region: "auto",
         params: {
-          Bucket: env('R2_BUCKET'),
-          ACL: 'public-read', // ESSENCIAL!
+          Bucket: env("R2_BUCKET"),
+          ACL: "public-read", // Obrigatório para acesso público
         },
       },
-      baseUrl: env('R2_PUBLIC_URL'),
+      baseUrl: env("R2_PUBLIC_URL"), // Domínio customizado (ex: https://storage.softmeat.com.br)
       actionOptions: {
         upload: {},
         uploadStream: {},
