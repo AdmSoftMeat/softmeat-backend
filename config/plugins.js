4842d3795bfcfefb71fe68675f89ed2120410cfe
AdmSoftMeat <smcloudfare@gmail.com>
2025-05-22 03:46:41 -0300

    migra 14

--- config/plugins.js
+++ config/plugins.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: '@strapi/provider-upload-aws-s3',
+      provider: 'aws-s3',
       providerOptions: {
         accessKeyId: env('R2_ACCESS_KEY'),
         secretAccessKey: env('R2_SECRET_KEY'),
@@ -9,15 +9,14 @@ module.exports = ({ env }) => ({
         region: 'auto',
         params: {
           Bucket: env('R2_BUCKET'),
-          ACL: 'public-read',
-        },
+          ACL: 'public-read'
+        }
       },
-      baseUrl: env('R2_PUBLIC_URL'),
       actionOptions: {
         upload: {},
         uploadStream: {},
-        delete: {},
-      },
-    },
-  },
+        delete: {}
+      }
+    }
+  }
 });
