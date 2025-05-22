23f826b307b640de349489fe8a2e321e54aee714
AdmSoftMeat <smcloudfare@gmail.com>
2025-05-22 03:18:32 -0300

    migra 12

--- config/plugins.js
+++ config/plugins.js
@@ -1,30 +1,23 @@
-const { S3Client } = require('@aws-sdk/client-s3');
-
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: 'aws-s3',
+      provider: '@strapi/provider-upload-aws-s3',
       providerOptions: {
-        s3Client: new S3Client({
-          region: 'auto',
-          endpoint: env('R2_ENDPOINT'),
-          credentials: {
-            accessKeyId: env('R2_ACCESS_KEY'),
-            secretAccessKey: env('R2_SECRET_KEY'),
-          },
-          forcePathStyle: true, // Obrigatório para R2
-        }),
+        accessKeyId: env('R2_ACCESS_KEY'),
+        secretAccessKey: env('R2_SECRET_KEY'),
+        endpoint: env('R2_ENDPOINT'),
+        region: 'auto',
         params: {
           Bucket: env('R2_BUCKET'),
           ACL: 'public-read',
-          Key: (file) => {
-            const model = file.related[0]?.ref || 'uploads';
-            const fileName = `${file.hash}${file.ext}`;
-            return `${model}/${file.id}/${fileName}`;
-          }
         },
-        baseUrl: env('R2_PUBLIC_URL'),
       },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+      baseUrl: env('R2_PUBLIC_URL'),
     },
   },
 });
