84ffc737fa2e8e1964dc900ad6bc4ea172457f86
AdmSoftMeat <smcloudfare@gmail.com>
2025-05-22 01:45:14 -0300

    migra 8

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,30 @@
-const path = require("path");
+const { S3Client } = require('@aws-sdk/client-s3');
 
 module.exports = ({ env }) => ({
   upload: {
     config: {
-      provider: "@strapi/provider-upload-aws-s3",
+      provider: 'aws-s3',
       providerOptions: {
-        s3Options: {
+        s3Client: new S3Client({
+          region: 'auto',
+          endpoint: env('R2_ENDPOINT'),
           credentials: {
-            accessKeyId: env("R2_ACCESS_KEY"),
-            secretAccessKey: env("R2_SECRET_KEY"),
+            accessKeyId: env('R2_ACCESS_KEY'),
+            secretAccessKey: env('R2_SECRET_KEY')
           },
-          endpoint: env("R2_ENDPOINT"),
-          region: env("R2_REGION", "auto"),
-          params: {
-            Bucket: env("R2_BUCKET"),
-            ACL: "public-read",
-          },
+          forcePathStyle: true // Necessário para Cloudflare R2
+        }),
+        params: {
+          Bucket: env('R2_BUCKET'),
+          ACL: 'public-read' // Garante permissão pública
         },
-        baseUrl: env("R2_PUBLIC_URL"),
+        baseUrl: env('R2_PUBLIC_URL') // URL pública do Cloudflare R2
       },
-    },
-  },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {}
+      }
+    }
+  }
 });
