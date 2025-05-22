2f895479e8930331b193252c4b75adbebf290422
AdmSoftMeat <smcloudfare@gmail.com>
2025-05-22 02:04:56 -0300

    migra 9

--- config/plugins.js
+++ config/plugins.js
@@ -6,25 +6,20 @@ module.exports = ({ env }) => ({
       provider: 'aws-s3',
       providerOptions: {
         s3Client: new S3Client({
-          region: 'auto',
+          region: 'auto', // Cloudflare R2 requer 'auto'
           endpoint: env('R2_ENDPOINT'),
           credentials: {
             accessKeyId: env('R2_ACCESS_KEY'),
-            secretAccessKey: env('R2_SECRET_KEY')
+            secretAccessKey: env('R2_SECRET_KEY'),
           },
-          forcePathStyle: true // Necessário para Cloudflare R2
+          forcePathStyle: true, // Obrigatório para R2
         }),
         params: {
           Bucket: env('R2_BUCKET'),
-          ACL: 'public-read' // Garante permissão pública
+          ACL: 'public-read',
         },
-        baseUrl: env('R2_PUBLIC_URL') // URL pública do Cloudflare R2
+        baseUrl: env('R2_PUBLIC_URL'),
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {}
-      }
-    }
-  }
+    },
+  },
 });
