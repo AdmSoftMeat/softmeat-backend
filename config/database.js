a68b12a6f5dfd7686d022582f4b1719ce97e9918
AdmSoftMeat <smcloudfare@gmail.com>
2025-05-22 21:47:35 -0300

    migra start

--- config/database.js
+++ config/database.js
@@ -3,13 +3,13 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('PGHOST', 'localhost'),
-      port: env.int('PGPORT', 5432),
+      port: env.int('PGPORT', 5432), // Garante conversão para número
       database: env('PGDATABASE', 'strapi'),
       user: env('PGUSER', 'postgres'),
       password: env('PGPASSWORD', 'postgres'),
-      ssl: env.bool('DATABASE_SSL', false)
-        ? { rejectUnauthorized: false }
-        : false,
+      ssl: env.bool('DATABASE_SSL', true) ? {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false)
+      } : false,
     },
     debug: false,
   },
